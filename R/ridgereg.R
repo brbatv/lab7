f2cfe3d9ad8165d560f7b59bb74bebccfb612d19
Alessia De Biase <alede379@student.liu.se>
2017-10-13 17:47:49 +0200

    Regression coefficients and fitted values implemented

--- R/ridgereg.R
+++ R/ridgereg.R
@@ -9,17 +9,36 @@
 #'@exportClass ridgereg
 #'@export ridgereg
 
-ridgereg<-setRefClass("ridgereg",fields=list(formula="forumula",beta_ridge="numeric",y_hat="numeric",name_of_data_input="character"),
+#data<-iris
+#formula<-Petal.Length ~ Sepal.Width + Sepal.Length
+#formula<-Petal.Length ~ Species
+
+#a<-lm.ridge(formula,iris,lambda = 2,standardize = FALSE)
+#a$coef
+
+#ridgereg$new(formula=Petal.Length ~ Sepal.Width + Sepal.Length,data=iris,lambda=2)
+
+ridgereg<-setRefClass("ridgereg",fields=list(formula="formula",beta_ridge="matrix",y_hat="matrix",name_of_data_input="character"),
                      methods=list(
                        initialize=function(formula,data,lambda)
                        { 
                          name_of_data_input<<-deparse(substitute(data))
                          formula<<-formula
                          
+                         dependent_variable_name<-all.vars(formula)[1]
+                         Y<-data[[dependent_variable_name]] # vector of dependent variable
+                         Y<-as.matrix(Y,ncol=1,nrow=length(Y))
+                         X<-model.matrix(formula,data)[,-1] # matric of independent variables without intercept beta0
+                         n<-dim(X)[2]
                          
+                         X_norm<-matrix()
+                         X_norm<-scale(X,center=TRUE,scale=TRUE) #standardized covariates
                          
+                         beta_ridge <<- t(solve((t(X_norm) %*% X_norm) + (lambda*diag(rep(1,n))))%*%t(X_norm)%*%Y) #formula for beta_ridge
                          
-                       },
+                         y_hat <<- X %*% t(beta_ridge)                                               
+                         
+                       } ,
                        print=function()
                        {
                          "Prints the input and the coefficients in a user-friedly way"
