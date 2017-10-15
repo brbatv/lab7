c46a891e5c06f4fa924acb9d8806ffd100dad36e
brbatv <barakatbruno@gmail.com>
2017-10-16 01:02:22 +0200

    pass

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
+[![Build Status](https://travis-ci.org/brbatv/lab7.svg?branch=master)](https://travis-ci.org/brbatv/lab7)
 # lab7
 lab7 package
--- vignettes/ridgereg.R
+++ vignettes/ridgereg.R
@@ -1,5 +1,36 @@
 ## ----include=FALSE-------------------------------------------------------
 library(devtools)
-install_github("brbatv/lab7",force=TRUE)
+#install_github("brbatv/lab7",force=TRUE)
 library(lab7ab)
+library(MASS)
+library(mlbench)
+library(caret)
+library(leaps)
+data("BostonHousing")
+
+## ------------------------------------------------------------------------
+training_data <- createDataPartition(BostonHousing$crim,p = 0.55)
+
+training <- BostonHousing[training_data$Resample1, ]
+test <- BostonHousing[-training_data$Resample1, ]
+
+## ----results:hide--------------------------------------------------------
+full_model <- lm(crim~.,data=training)
+
+
+## ----include=FALSE-------------------------------------------------------
+
+step <- stepAIC(full_model, direction="backward")
+
+## ----eval=FALSE,warning=FALSE--------------------------------------------
+#  
+#  step <- stepAIC(full_model, direction="backward")
+
+## ----warning=FALSE-------------------------------------------------------
+step$anova # display results 
+
+## ----warning=FALSE-------------------------------------------------------
+lm1 <- caret::train(crim ~ ., data=training, method = 'leapForward')
+lm1
+summary(lm1)
 
